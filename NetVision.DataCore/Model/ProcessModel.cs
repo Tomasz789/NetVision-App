dcb4d2c3228f7d8d970167e907e4377ae682f7ee
Tomasz789 <lachowiec_95@wp.pl>
2022-07-02 22:39:43 +0200

    A new model added

---
+++ NetVision.DataCore/Model/ProcessModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetVision.DataCore.Model
+{
+    public class ProcessModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public long Memory { get; set; }
+    }
+}
