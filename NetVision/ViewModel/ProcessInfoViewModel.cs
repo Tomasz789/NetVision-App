dbbf7c6f92b5e652580e1aa580db85cdcd297ceb
Tomasz789 <lachowiec_95@wp.pl>
2022-07-02 22:45:08 +0200

    VM for Process feature

---
+++ NetVision/ViewModel/ProcessInfoViewModel.cs
@@ -0,0 +1,76 @@
+﻿using NetVision.DataCore.Model;
+using NetVision.Infrastructure.Services;
+using NetVision.MVRelayCmds;
+using NetVision.ViewModel.PropertyUpdater;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NetVision.ViewModel
+{
+    public class ProcessInfoViewModel : ViewModelPropertyUpdater
+    {
+        private int min, max;
+        private IEnumerable<ProcessModel> procModel;
+        private readonly IProcessInfoService _service;
+
+        public ProcessInfoViewModel()
+        {
+            this.min = 0;
+            this.max = 0;
+            _service = new ProcessInfoService();
+            procModel = _service.GetProcesses().AsEnumerable();    //get all process list
+            SearchProcessesCmd = new RelayCommand(GetList, CanExecute);
+        }
+
+        public ICommand SearchProcessesCmd { get; set; }
+
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                min = value;
+                OnPropertyChanged("Min");
+            }
+        }
+        public int Max
+        {
+            get
+            {
+                return max;
+            }
+            set
+            {
+                max = value;
+                OnPropertyChanged("Max");
+            }
+        }
+        public IEnumerable<ProcessModel> ProcessList
+        {
+            get
+            {
+                return procModel;
+            }
+            set
+            {
+                procModel = value;
+                OnPropertyChanged("ProcessList");
+            }
+        }
+
+        public bool CanExecute(object parameter) { return true; }
+
+        public void GetList(object param)
+        {
+            ProcessList = _service.SearchInIdRange(this.min, this.max, procModel);
+        }
+
+    }
+}
