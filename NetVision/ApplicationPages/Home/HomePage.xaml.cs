fad0d040d3cd415a689940e6900cce7b2d2096e3
Tomasz789 <lachowiec_95@wp.pl>
2022-07-03 23:47:10 +0200

    Added button for process launching

--- NetVision/ApplicationPages/Home/HomePage.xaml.cs
+++ NetVision/ApplicationPages/Home/HomePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NetVision.Infrastructure.Services.ProcessService;
+using NetVision.Infrastructure.Services.ProcessService.Types;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,65 @@ namespace NetVision.ApplicationPages.Home
     /// </summary>
     public partial class HomePage : Page
     {
+        private readonly List<Button> buttonList;
         public HomePage()
         {
             InitializeComponent();
+            buttonList = new List<Button>();
+            buttonList.Add(cmdBtn);
+            buttonList.Add(controlBtn);
+            buttonList.Add(fileExplorerBtn);
+            buttonList.Add(devBtn);
+            buttonList.Add(taskBtn);
+
+            foreach (var btn in buttonList)
+            {
+                btn.Click += new RoutedEventHandler(ButtonSelected);
+            }
+        }
+
+        private void ButtonSelected(object sender, RoutedEventArgs e)
+        {
+            var selectedButton = e.Source as Button;
+            ProcessLauncher launcher = null;
+
+            switch (selectedButton.Name)
+            {
+                case "cmdBtn":
+                    {
+                        launcher = new ProcessLauncher(ProcessTypes.CMD);
+                    }
+                    break;
+                case "controlBtn":
+                    {
+                        launcher = new ProcessLauncher(ProcessTypes.CONTROL);
+                    }
+                    break;
+                case "fileExplorerBtn":
+                    {
+                        launcher = new ProcessLauncher(ProcessTypes.EXPLORER);
+                    }
+                    break;
+                case "devBtn":
+                    {
+                        launcher = new ProcessLauncher(ProcessTypes.DEVMGMT);
+                    }
+                    break;
+                case "taskBtn":
+                    {
+                        launcher = new ProcessLauncher(ProcessTypes.TASKMGR);
+                    }
+                    break;
+            }
+
+            try
+            {
+                launcher.LaunchProcess();
+            }
+            catch (System.ComponentModel.Win32Exception winEx)
+            {
+                MessageBox.Show(winEx.Message);
+            }
         }
     }
 }
