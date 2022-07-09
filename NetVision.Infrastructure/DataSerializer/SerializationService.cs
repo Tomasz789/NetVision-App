a15057c76183b23ab0eec6181498d07c0409f07e
Tomasz789 <lachowiec_95@wp.pl>
2022-07-09 12:16:18 +0200

    Service puropsed for xml operations

---
+++ NetVision.Infrastructure/DataSerializer/SerializationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetVision.Infrastructure.DataSerializer
+{
+    public interface ISerializationService<T> where T : class
+    {
+        public int SerializeData(T dataObject, string path);
+        public T DeserializeData(string path);
+        public bool ValidateData(T dataObject);
+    }
+}
---
+++ NetVision.Infrastructure/DataSerializer/XmlSerializationHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace NetVision.Infrastructure.DataSerializer
+{
+    public sealed class XmlSerializationHelper<T> : ISerializationService<T> where T : class
+    {
+        public T DeserializeData(string path)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            using (var reader = new StreamReader(path))
+            {
+                var objectToReturn = serializer.Deserialize(reader);
+                return objectToReturn as T;
+            }
+        }
+
+        public int SerializeData(T dataObject, string path)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            using (var writer = new StreamWriter(path))
+            {
+                serializer.Serialize(writer, dataObject);
+                writer.Close();
+            }
+
+            return 1;
+        }
+
+        public bool ValidateData(T dataObject)
+        {
+            if (dataObject is not T)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
