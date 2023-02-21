9c45ecc7da87a26d5a95e7f1b674a90a2ba8d621
Tomasz789 <lachowiec_95@wp.pl>
2023-02-21 18:48:20 +0100

    Exceptions for hosts

---
+++ NetVision.DataException/Exceptions/IPAddressException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetVision.DataException
+{
+    public class IPAddressException : Exception
+    {
+        public IPAddressException(string value) : base(value) 
+        {
+
+        }
+    }
+}
---
+++ NetVision.DataException/Exceptions/InvalidHostException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetVision.DataException.Exceptions
+{
+    public class InvalidHostException : Exception
+    {
+        public InvalidHostException(string host) 
+            : base(string.Format("Invalid host format {0}", host))
+        {
+
+        }
+    }
+}
