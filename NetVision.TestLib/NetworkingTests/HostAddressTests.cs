78ec0360ca9d077a1071c200a40ee6da32f903c1
Tomasz789 <lachowiec_95@wp.pl>
2023-02-21 16:44:47 +0100

    Tests for IP and host methods

---
+++ NetVision.TestLib/NetworkingTests/HostAddressTests.cs
@@ -0,0 +1,46 @@
+﻿using NetVision.Infrastructure.Services;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetVision.TestLib.NetworkingTests
+{
+    [TestFixture]
+    public class HostAddressTests
+    {
+        private IPingInfoService _service;
+        private readonly string hostname = "www.google.com";
+        private readonly string hostaddress = "216.58.215.100";
+
+        [SetUp]
+        public void Init()
+        {
+            _service = new PingInfoService();
+        }
+
+        [Test]
+
+        public void GetHostNameForAddress_Test()
+        {
+            var ip = IPAddress.Parse(hostaddress);
+            var addr = _service.GetNameFromAddress(ip);
+
+            Assert.IsNotNull(addr);
+            Assert.AreEqual(addr, hostname);
+        }
+
+        [Test]
+
+        public void GetAddressFromHostName_Test()
+        {
+            var hostIp = _service.GetAddressFromHostName(hostname).ToString();
+
+            Assert.IsNotNull(hostIp);
+            Assert.AreEqual(hostIp, hostaddress);
+        }
+    }
+}
